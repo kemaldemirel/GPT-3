913ae9bf1dd6dc444e9c83891f5e45b44b6df43c
Кемал Аджимамбетов <demirelkd0696@gmail.com>
2021-11-14 16:39:40 +0300

    add structur files and basic style

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Article from './article/Article';
+import Brand from './brand/Brand';
+import CTA from './cta/CTA';
+import Features from './features/Features';
+import Navbar from './navbar/Navbar';
+
+export { Article, Brand, CTA, Features, Navbar };
